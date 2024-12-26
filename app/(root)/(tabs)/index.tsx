a83dafff754c6968b9845f451ade675193a009ce
pranjal482 <ipisc0051@pandoconsultants.com>
2024-12-26 23:41:44 +0530

    Sign splash completed

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,21 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="my-5  font-rubik-extrabold text-2xl">Welcome to Restate</Text>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/sign-in">Sign In</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href="/properties/1">Property</Link>
+    </View>
+  );
+}
